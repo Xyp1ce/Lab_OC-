6db50f1efedcb67ea20f59346877df19d6a4dbfa
RamonManriquez4559 <manriquez.ramon@uabc.edu.mx>
2025-11-05 06:17:06 +0000

    Practica 9 terminada

--- P9/P9.asm
+++ P9/P9.asm
@@ -6,45 +6,44 @@ section .data
     msg db "Ingresa un dato", 0xa, 0
     msg1 db "Mostrando datos", 0xa, 0
     msg2 db "Realizando suma de vectores", 0xa, 0
-    msg3 db "Realizando producto punto de vecotres", 0xa, 0
-    N equ 2
+    msg3 db "Realizando producto punto de vectores", 0xa, 0
+    N equ 10
 
 section .bss
-    ; Reservar memoria para dos arreglos con tamanno de 10 bytes cada uno
+    ; Reservar memoria para dos arreglos con tamaño de 10 bytes cada uno
     array1 resb N
     array2 resb N
+    result resb N
 
 section .text  
     global _start
 
 _start:
-    mov ebx, array1 ; Pasar la direccion de array1 a ebx
-    mov ecx, N ; N vale 10 
+    mov ebx, array1 ; Pasar la direccion de array1 a 'ebx'
     mov edx, msgAr1
     call puts
     call _LeeVector
-    mov ecx, N
     call _MostrarVector
-    mov ebx, array2 ; Pasar la direccin de array2 a ebx
-    mov ecx, N ; Reiniciar el valor de N
+    mov ebx, array2 ; Pasar la direccin de array2 a 'ebx'
     mov edx, msgAr2
     call puts
     call _LeeVector
-    mov ecx, N
     call _MostrarVector
     call _SumarVector
+    call _ProductoPunto
     mov eax, 1
     int 0x80
 
 _lf: ; Procedimiento para hacer un line feed
-    push ax
+    push eax
     mov al, 10
     call putchar
-    pop ax
+    pop eax
     ret
 
-_LeeVector: ; Procedimiento para leer N veces un dato y almacenarlo en el arreglo que contiene ebx
-    mov esi, 1
+_LeeVector: ; Procedimiento para leer N veces un dato y almacenarlo en el arreglo que contiene 'ebx'
+    mov ecx, N
+    mov esi, 0
     mov edx, msg
     .loop:
         call puts
@@ -64,10 +63,11 @@ _LeeVector: ; Procedimiento para leer N veces un dato y almacenarlo en el arregl
     loop .loop
     ret
 
-_MostrarVector: ; Procedimiento para mostrar los N datos del arreglo almacenado en ebx
+_MostrarVector: ; Procedimiento para mostrar los N datos del arreglo almacenado en 'ebx'
+    mov ecx, N
     mov edx, msg1
     call puts
-    mov esi, 1
+    mov esi, 0
     .loop:
         mov al, [ebx + esi]
         call pHex_b
@@ -76,13 +76,39 @@ _MostrarVector: ; Procedimiento para mostrar los N datos del arreglo almacenado 
     loop .loop
     ret
 
-_SumarVector: ; Procedimiento para sumar dos vectores almacenados en ebx y edx
+_SumarVector: ; Procedimiento para sumar dos vectores almacenados en 'ebx' y 'edx', el resultado se guarda en 'edi'
+    mov ecx, N
     mov edx, msg2
     call puts
+    mov esi, 0
     mov ebx, array1
     mov edx, array2
-
+    mov edi, result
+    .sumar:
+        mov al, [edx + esi]
+        add al, [ebx+esi]
+        mov [edi + esi], al 
+        inc esi
+    loop .sumar
+    mov ebx, result
+    call _MostrarVector
     ret
 
-_ProductoPunto: ; Procedimiento para calcular el producto punto de dos arreglos almacenados en ebx y edx
+_ProductoPunto: ; Procedimiento para calcular el producto punto de dos arreglos almacenados en 'ebx' y 'edx', el resultado se guarda en 'ebx'
+    mov ecx, N
+    mov edx, msg3
+    call puts
+    mov esi, 0
+    mov ebx, array1
+    mov edx, array2
+    mov edi, 0
+    .mult:
+        mov al, [ebx+esi]
+        mul byte[edx+esi] ; El resultado se guarda en 'ax'
+        add di, ax
+        inc esi
+    loop .mult
+    mov ax, di
+    call pHex_b
+    call _lf
     ret
